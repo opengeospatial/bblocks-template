719bd74b7bc048fa1e820ae0bbcc07b4b84c1dcd
Alejandro Villar <avillar@ogc.org>
2026-09-09 14:56:34 +0200

    Forward extra CLI args from build.sh to the postprocessor
    Lets ./build.sh (and build-devel.sh, which execs it) accept flags like
    --filter/--steps for finer-grained local iteration, instead of only
    ever running the hardcoded full clean build.

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ if [ -f '.volumes' ]; then
 fi
 docker run -it --pull=always --rm --workdir /workspace -v "$(pwd):/workspace" ${VOLUMES} \
   ghcr.io/opengeospatial/bblocks-postprocess:${BBP_IMAGE_TAG:-latest} \
-  --clean true --base-url http://localhost:9090/register/
+  --clean true --base-url http://localhost:9090/register/ "$@"
